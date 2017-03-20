6e5b2183be3b910f0d366acdb06648b7d1a7ed6b
tianma <1808931265@qq.com>
2017-03-20 16:13:53 +0800

    Add the function of permission check, and modify the function of getting real path by Uri.

--- app/src/main/java/com/tianma/photochooser/MainActivity.java
+++ app/src/main/java/com/tianma/photochooser/MainActivity.java
@@ -1,19 +1,28 @@
 package com.tianma.photochooser;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tianma.photochooser.image.ImageUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CHOOSE_IMAGE = 0x01;
+    private static final int REQUEST_WRITE_PERMISSION_GRANT = 0x02;
 
     private TextView photoPath;
     private ImageView photo;
@@ -26,11 +35,20 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.choose_image_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startSelectPhoto();
+                final String WRITE_PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+                if (permissionCheck(MainActivity.this, WRITE_PERMISSION)) {
+                    startSelectPhoto();
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{WRITE_PERMISSION}, REQUEST_WRITE_PERMISSION_GRANT);
+                }
             }
         });
         photoPath = (TextView) findViewById(R.id.photo_path);
         photo = (ImageView) findViewById(R.id.photo);
+    }
+
+    private boolean permissionCheck(Context context, String permission) {
+        return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
     }
 
     private void startSelectPhoto() {
@@ -44,13 +62,31 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == REQUEST_CHOOSE_IMAGE && resultCode == RESULT_OK) {
             Uri uri =  data.getData();
             String path = ImageUtils.getRealPathFromUri(this, uri);
-            photoPath.setVisibility(View.VISIBLE);
-            photoPath.setText(path);
-            int requiredHeight = photo.getHeight();
-            int requiredWidth = photo.getWidth();
-            Bitmap bm = ImageUtils.decodeSampledBitmapFromDisk(path, requiredWidth, requiredHeight);
-            photo.setImageBitmap(bm);
+            if (!TextUtils.isEmpty(path)) {
+                photoPath.setVisibility(View.VISIBLE);
+                photoPath.setText(path);
+                int requiredHeight = photo.getHeight();
+                int requiredWidth = photo.getWidth();
+                Bitmap bm = ImageUtils.decodeSampledBitmapFromDisk(path, requiredWidth, requiredHeight);
+                photo.setImageBitmap(bm);
+            } else {
+                Toast.makeText(this, "Fail to get image", Toast.LENGTH_SHORT).show();
+            }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_WRITE_PERMISSION_GRANT:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startSelectPhoto();
+                } else {
+                    Toast.makeText(MainActivity.this, "You denied the write_external_storage permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
--- app/src/main/java/com/tianma/photochooser/image/ImageUtils.java
+++ app/src/main/java/com/tianma/photochooser/image/ImageUtils.java
@@ -1,11 +1,8 @@
 package com.tianma.photochooser.image;
 
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-
-import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Context;
-import android.content.CursorLoader;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -15,7 +12,9 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
-import android.util.Log;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
 
 /**
  * 与图片操作相关的工具类
@@ -111,12 +110,9 @@ public class ImageUtils {
     /**
      * 根据传入的BitmapFactory.Options对象和图片需要的宽和高计算出压缩比例
      *
-     * @param options
-     *            BitmapFactory.Options对象
-     * @param requiredWidth
-     *            需要的图片的宽度
-     * @param requiredHeight
-     *            需要的图片的高度
+     * @param options        BitmapFactory.Options对象
+     * @param requiredWidth  需要的图片的宽度
+     * @param requiredHeight 需要的图片的高度
      * @return
      */
     private static int calculateInSampleSize(BitmapFactory.Options options,
@@ -137,111 +133,174 @@ public class ImageUtils {
         return inSampleSize;
     }
 
-    /**
-     * 根据图片的Uri获取图片的绝对路径(已经适配多种API)
-     *
-     * @param context
-     *            上下文对象
-     * @param uri
-     *            图片的Uri
-     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
-     */
-    public static String getRealPathFromUri(Context context, Uri uri) {
-        Log.d("Tianma", "Uri = " + uri);
-        int sdkVersion = Build.VERSION.SDK_INT;
-        if (sdkVersion < 11) {
-            // SDK < Api11
-            return getRealPathFromUri_BelowApi11(context, uri);
-        }
-        if (sdkVersion < 19) {
-            // SDK > 11 && SDK < 19
-            return getRealPathFromUri_Api11To18(context, uri);
-        }
-        // SDK > 19
-        return getRealPathFromUri_AboveApi19(context, uri);
-    }
+//    /**
+//     * 根据图片的Uri获取图片的绝对路径(已经适配多种API)
+//     *
+//     * @param context
+//     *            上下文对象
+//     * @param uri
+//     *            图片的Uri
+//     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
+//     */
+//    public static String getRealPathFromUri(Context context, Uri uri) {
+//        Log.d("Tianma", "Uri = " + uri);
+//        int sdkVersion = Build.VERSION.SDK_INT;
+//        if (sdkVersion < 11) {
+//            // SDK < Api11
+//            return getRealPathFromUri_BelowApi11(context, uri);
+//        }
+//        if (sdkVersion < 19) {
+//            // SDK >= 11 && SDK < 19
+//            return getRealPathFromUri_Api11To18(context, uri);
+//        }
+//        // SDK >= 19
+//        return getRealPathFromUri_AboveApi19(context, uri);
+//    }
+//
+//    /**
+//     * 适配api19以上,根据uri获取图片的绝对路径
+//     *
+//     * @param context
+//     *            上下文对象
+//     * @param uri
+//     *            图片的Uri
+//     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
+//     */
+//    @SuppressLint("NewApi")
+//    private static String getRealPathFromUri_AboveApi19(Context context, Uri uri) {
+//        String filePath = null;
+//        String wholeID = DocumentsContract.getDocumentId(uri);
+//
+//        Log.d("Tianma", "wholeId = " + wholeID);
+//        // 使用':'分割
+//        String id = wholeID.split(":")[1];
+//
+//        String[] projection = { MediaStore.Images.Media.DATA };
+//        String selection = MediaStore.Images.Media._ID + "=?";
+//        String[] selectionArgs = { id };
+//
+//        Cursor cursor = context.getContentResolver().query(
+//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection,
+//                selection, selectionArgs, null);
+//        int columnIndex = cursor.getColumnIndex(projection[0]);
+//
+//        if (cursor.moveToFirst()) {
+//            filePath = cursor.getString(columnIndex);
+//        }
+//        cursor.close();
+//        return filePath;
+//    }
+//
+//    /**
+//     * 适配api11-api18,根据uri获取图片的绝对路径
+//     *
+//     * @param context
+//     *            上下文对象
+//     * @param uri
+//     *            图片的Uri
+//     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
+//     */
+//    private static String getRealPathFromUri_Api11To18(Context context, Uri uri) {
+//        String filePath = null;
+//        String[] projection = { MediaStore.Images.Media.DATA };
+//
+//        CursorLoader loader = new CursorLoader(context, uri, projection, null,
+//                null, null);
+//        Cursor cursor = loader.loadInBackground();
+//
+//        if (cursor != null) {
+//            cursor.moveToFirst();
+//            filePath = cursor.getString(cursor.getColumnIndex(projection[0]));
+//            cursor.close();
+//        }
+//
+//        return filePath;
+//
+//    }
+//
+//    /**
+//     * 适配api11以下(不包括api11),根据uri获取图片的绝对路径
+//     *
+//     * @param context
+//     *            上下文对象
+//     * @param uri
+//     *            图片的Uri
+//     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
+//     */
+//    private static String getRealPathFromUri_BelowApi11(Context context, Uri uri) {
+//        String filePath = null;
+//        String[] projection = { MediaStore.Images.Media.DATA };
+//        Cursor cursor = context.getContentResolver().query(uri, projection,
+//                null, null, null);
+//        if (cursor != null) {
+//            cursor.moveToFirst();
+//            filePath = cursor.getString(cursor.getColumnIndex(projection[0]));
+//            cursor.close();
+//        }
+//        return filePath;
+//    }
 
     /**
      * 适配api19以上,根据uri获取图片的绝对路径
      *
-     * @param context
-     *            上下文对象
-     * @param uri
-     *            图片的Uri
-     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
+     * @param context 上下文对象
+     * @param uri     图片的Uri
+     * @return 如果Uri对应的图片存在, 那么返回该图片的绝对路径, 否则返回null
      */
-    @SuppressLint("NewApi")
-    private static String getRealPathFromUri_AboveApi19(Context context, Uri uri) {
-        String filePath = null;
-        String wholeID = DocumentsContract.getDocumentId(uri);
-
-        Log.d("Tianma", "wholeId = " + wholeID);
-        // 使用':'分割
-        String id = wholeID.split(":")[1];
-
-        String[] projection = { MediaStore.Images.Media.DATA };
-        String selection = MediaStore.Images.Media._ID + "=?";
-        String[] selectionArgs = { id };
-
-        Cursor cursor = context.getContentResolver().query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection,
-                selection, selectionArgs, null);
-        int columnIndex = cursor.getColumnIndex(projection[0]);
-
-        if (cursor.moveToFirst()) {
-            filePath = cursor.getString(columnIndex);
+    public static String getRealPathFromUri(Context context, Uri uri) {
+        int sdkVersion = Build.VERSION.SDK_INT;
+        if (sdkVersion < 19) { // < 19
+            return getRealPathFromUriBelowAPI19(context, uri);
+        } else { // >= 19
+            return getRealPathFromUriAboveAPI19(context, uri);
         }
-        cursor.close();
-        return filePath;
+    }
+
+    // < 19
+    private static String getRealPathFromUriBelowAPI19(Context context, Uri uri) {
+        return getDataColumn(context, uri, null, null);
+    }
+
+    // >= 19
+    @TargetApi(19)
+    private static String getRealPathFromUriAboveAPI19(Context context, Uri uri) {
+        String imagePath = null;
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            // document 类型的 Ui, 需要通过 document id 处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1]; // 解析出 id
+                String selection = MediaStore.Images.Media._ID + "=?";
+                String[] selectionArgs = new String[]{id};
+                imagePath = getDataColumn(context, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection, selectionArgs);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                imagePath = getDataColumn(context, contentUri, null, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // content 类型的 Uri，使用普通方式处理
+            imagePath = getDataColumn(context, uri, null, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            // file 类型的 Uri，直接获取图片路径
+            imagePath = uri.getPath();
+        }
+        return imagePath;
     }
 
     /**
-     * 适配api11-api18,根据uri获取图片的绝对路径
-     *
-     * @param context
-     *            上下文对象
-     * @param uri
-     *            图片的Uri
-     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
+     * 获取 _data 列对应的内容, 其实就是 path
+     * @return
      */
-    private static String getRealPathFromUri_Api11To18(Context context, Uri uri) {
-        String filePath = null;
-        String[] projection = { MediaStore.Images.Media.DATA };
-
-        CursorLoader loader = new CursorLoader(context, uri, projection, null,
-                null, null);
-        Cursor cursor = loader.loadInBackground();
-
-        if (cursor != null) {
-            cursor.moveToFirst();
-            filePath = cursor.getString(cursor.getColumnIndex(projection[0]));
+    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        String dataColumn = MediaStore.Images.Media.DATA;
+        String[] projection = new String[]{dataColumn};
+        Cursor cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+        String path = null;
+        if (cursor != null && cursor.moveToFirst()) {
+            path = cursor.getString(cursor.getColumnIndex(dataColumn));
             cursor.close();
         }
-
-        return filePath;
-
-    }
-
-    /**
-     * 适配api11以下(不包括api11),根据uri获取图片的绝对路径
-     *
-     * @param context
-     *            上下文对象
-     * @param uri
-     *            图片的Uri
-     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
-     */
-    private static String getRealPathFromUri_BelowApi11(Context context, Uri uri) {
-        String filePath = null;
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = context.getContentResolver().query(uri, projection,
-                null, null, null);
-        if (cursor != null) {
-            cursor.moveToFirst();
-            filePath = cursor.getString(cursor.getColumnIndex(projection[0]));
-            cursor.close();
-        }
-        return filePath;
+        return path;
     }
 
 }
